f48b68680a7e37e2c3f08e37dd3d2f6d363dd897
Michael Vorburger <mike@vorburger.ch>
2014-04-01 00:07:54 +0200

    removing executeScript.sh, which is no longer used now (thanks to @osimola https://github.com/vorburger/MariaDB4j/pull/8/)

--- src/main/resources/ch/vorburger/mariadb4j/mariadb-5.5.33a/linux/executeScript.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-input="$1"
-shift
-./bin/mysql --no-defaults -u root -D test  "$@" < "$input"
-
--- src/main/resources/ch/vorburger/mariadb4j/mariadb-5.5.34/osx/executeScript.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-input="$1"
-shift
-./bin/mysql --no-defaults -u root -D test  "$@" < "$input"
