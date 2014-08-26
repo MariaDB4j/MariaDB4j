6f572560ceb9908c50883696480f42483940e1ab
Michael Vorburger <mike@vorburger.ch>
2014-08-26 22:50:48 +0200

    StartManyTimes main() which reproduces https://github.com/vorburger/MariaDB4j/issues/10
    Signed-off-by: Michael Vorburger <mike@vorburger.ch>

---
+++ src/test/java/ch/vorburger/mariadb4j/tests/StartManyTimes.java
@@ -0,0 +1,32 @@
+package ch.vorburger.mariadb4j.tests;
+
+import ch.vorburger.exec.ManagedProcessException;
+import ch.vorburger.mariadb4j.DB;
+import ch.vorburger.mariadb4j.DBConfiguration;
+import ch.vorburger.mariadb4j.DBConfigurationBuilder;
+
+/**
+ * Reproduces issue #10.
+ * 
+ * @see https://github.com/vorburger/MariaDB4j/issues/10
+ * 
+ * @author Michael Vorburger
+ */
+public class StartManyTimes {
+
+    // TODO Fix this problem (and remove this)
+
+    public static void main(String[] args) throws ManagedProcessException {
+        DBConfigurationBuilder configBuilder = DBConfigurationBuilder.newBuilder();
+        configBuilder.detectFreePort();
+        DBConfiguration config = configBuilder.build();
+
+        for (int i = 0; i < 100000; i++) {
+            DB db = DB.newEmbeddedDB(config);
+            db.start();
+            db.stop();
+            System.out.println(i);
+        }
+    }
+
+}
