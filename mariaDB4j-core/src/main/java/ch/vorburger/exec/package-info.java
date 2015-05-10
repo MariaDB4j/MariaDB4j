867d7f436a306a90cd1ce5284263be0fa60a21ae
Michael Vorburger <mike@vorburger.ch>
2015-05-10 17:11:22 +0200

    Issue #3, link to zt-exec. Also see https://github.com/zeroturnaround/zt-exec/pull/25.
    Signed-off-by: Michael Vorburger <mike@vorburger.ch>

---
+++ mariaDB4j-core/src/main/java/ch/vorburger/exec/package-info.java
@@ -0,0 +1,31 @@
+/*
+ * #%L
+ * MariaDB4j
+ * %%
+ * Copyright (C) 2015 Michael Vorburger
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+/**
+ * Improvements over Apache Commons Exec.
+ * Proposed as contribution to Apache Commons Exec in <a href="https://issues.apache.org/jira/browse/EXEC-88">EXEC-88</a>.
+ * 
+ * <p>The <a href="https://github.com/zeroturnaround/zt-exec">zt-exec</a> library is something similar.</p>
+ * 
+ * @see ch.vorburger.exec.ManagedProcessBuilder 
+ * @see ch.vorburger.exec.ManagedProcess
+ * 
+ * @author Michael Vorburger
+ */
+package ch.vorburger.exec;
