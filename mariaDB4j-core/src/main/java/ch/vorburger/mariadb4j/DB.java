0ab0cc664898a2193dbcae1e6daa1aee380ceff3
carlos.ortiz@craftersoftware.com <chrono.dark@gmail.com>
2017-06-22 16:02:42 -0600

    Add myself (Carlos Ortiz)
    and two minor clean-up changes tacked on, just for simplicity

--- mariaDB4j-core/src/main/java/ch/vorburger/mariadb4j/DB.java
+++ mariaDB4j-core/src/main/java/ch/vorburger/mariadb4j/DB.java
@@ -2,14 +2,14 @@
  * #%L
  * MariaDB4j
  * %%
- * Copyright (C) 2012 - 2014 Michael Vorburger
+ * Copyright (C) 2012 - 2017 Michael Vorburger
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
