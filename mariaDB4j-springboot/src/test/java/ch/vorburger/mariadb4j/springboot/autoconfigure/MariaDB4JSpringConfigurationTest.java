9d9aa0b67f0d1ae9a43629138ca6f5a7890eb898
kbyyd24 <melo@gaoyuexiang.cn>
2018-08-25 18:25:18 +0800

    [#153] fixed info in copyright

--- mariaDB4j-springboot/src/test/java/ch/vorburger/mariadb4j/springboot/autoconfigure/MariaDB4JSpringConfigurationTest.java
+++ mariaDB4j-springboot/src/test/java/ch/vorburger/mariadb4j/springboot/autoconfigure/MariaDB4JSpringConfigurationTest.java
@@ -2,7 +2,7 @@
  * #%L
  * MariaDB4j
  * %%
- * Copyright (C) 2012 - 2014 Michael Vorburger
+ * Copyright (C) 2012 - 2018 Yuexiang Gao
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
