420b352e82955202823e06525f7117b274ed6c21
Michael Vorburger <mike@vorburger.ch>
2018-08-28 20:56:52 +0200

    minor: make newly introduced method public getProcessListener()
    instead of protected _getManagedProcessListener()
    
    for consistency with rest of DBConfigurationBuilder

--- mariaDB4j-core/src/main/java/ch/vorburger/mariadb4j/DBConfigurationBuilder.java
+++ mariaDB4j-core/src/main/java/ch/vorburger/mariadb4j/DBConfigurationBuilder.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,12 +19,11 @@
  */
 package ch.vorburger.mariadb4j;
 
+import ch.vorburger.exec.ManagedProcessListener;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.util.ArrayList;
 import java.util.List;
-
-import ch.vorburger.exec.ManagedProcessListener;
 import org.apache.commons.lang3.SystemUtils;
 
 /**
@@ -127,6 +126,10 @@ public class DBConfigurationBuilder {
         return this;
     }
 
+    public ManagedProcessListener getProcessListener() {
+        return listener;
+    }
+
     public boolean isDeletingTemporaryBaseAndDataDirsOnShutdown() {
         return isDeletingTemporaryBaseAndDataDirsOnShutdown;
     }
@@ -170,7 +173,7 @@ public class DBConfigurationBuilder {
         frozen = true;
         return new DBConfiguration.Impl(_getPort(), _getSocket(), _getBinariesClassPathLocation(), getBaseDir(), getLibDir(), _getDataDir(),
                 WIN32.equals(getOS()), _getArgs(), _getOSLibraryEnvironmentVarName(), isSecurityDisabled(),
-                isDeletingTemporaryBaseAndDataDirsOnShutdown(), this::getURL, _getManagedProcessListener());
+                isDeletingTemporaryBaseAndDataDirsOnShutdown(), this::getURL, getProcessListener());
     }
 
     /**
@@ -281,11 +284,6 @@ public class DBConfigurationBuilder {
         else
             return null; // see ch.vorburger.mariadb4j.DB.unpackEmbeddedDb()
     }
-
-    protected ManagedProcessListener _getManagedProcessListener() {
-        return listener;
-    }
-
 
     public boolean isUnpackingFromClasspath() {
         return isUnpackingFromClasspath;
