663413673212140434ab9eedaf294c6ab9dcf88f
Anthoknee <cristianoanthony@gmail.com>
2019-11-27 18:27:26 -0800

    Rename package local installPreparation() method to createDBInstallProcess() and expose it as protected (#300)
    https://github.com/vorburger/MariaDB4j/pull/300
    
    TX https://github.com/Anthoknee

--- mariaDB4j/src/test/java/ch/vorburger/mariadb4j/StartSimulatedForAllPlatformsTest.java
+++ mariaDB4j/src/test/java/ch/vorburger/mariadb4j/StartSimulatedForAllPlatformsTest.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -32,10 +32,10 @@ import ch.vorburger.exec.ManagedProcessException;
 
 /**
  * Simulating starting MariaDB4j on all supported platforms.
- * 
+ *
  * <p>This detects the recurring issue of some mariaDB startup script not being where it's expected to
  * be and breaking a platform when upgrading the binaries or making code changes.
- * 
+ *
  * @author Michael Vorburger
  */
 public class StartSimulatedForAllPlatformsTest {
@@ -61,7 +61,7 @@ public class StartSimulatedForAllPlatformsTest {
         db.prepareDirectories();
         db.unpackEmbeddedDb();
 
-        ManagedProcess installProc = db.installPreparation();
+        ManagedProcess installProc = db.createDBInstallProcess();
         checkManagedProcessExists(installProc);
 
         ManagedProcess startProc = db.startPreparation();
