bfbfa4a93376e620931c590594de9e7e4589f4cb
Luigi Berrettini <luigi.berrettini@gmail.com>
2020-10-21 20:48:36 +0200

    Upgrade MariaDB 10.3.x to 10.3.25

--- mariaDB4j-core/src/main/java/ch/vorburger/mariadb4j/DBConfigurationBuilder.java
+++ mariaDB4j-core/src/main/java/ch/vorburger/mariadb4j/DBConfigurationBuilder.java
@@ -252,11 +252,11 @@ public class DBConfigurationBuilder {
         String databaseVersion = getDatabaseVersion();
         if (databaseVersion == null) {
             if (OSX.equals(getOS()))
-                databaseVersion = "mariadb-10.3.16";
+                databaseVersion = "mariadb-10.3.25";
             else if (LINUX.equals(getOS()))
-                databaseVersion = "mariadb-10.3.16";
+                databaseVersion = "mariadb-10.3.25";
             else if (WIN32.equals(getOS()))
-                databaseVersion = "mariadb-10.3.16";
+                databaseVersion = "mariadb-10.3.25";
             else
                 throw new IllegalStateException(
                         "OS not directly supported, please use setDatabaseVersion() to set the name "
