f272032c977b3cd298c5189336846d33cc3ff0cc
Michael Vorburger <mike@vorburger.ch>
2025-03-29 11:22:33 +0100

    ci: Fix https://pre-commit.com

---
+++ precommit.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+if ! [ -e ".venv/bin/pre-commit" ]; then
+  if ! [ -x "$(command -v python3)" ]; then
+    echo "python3 is not installed, please run e.g. 'sudo apt-get install virtualenv python3-venv' (or an equivalent)"
+    exit 255
+  fi
+
+  if ! [ -d .venv/ ]; then
+     python3 -m venv .venv
+  fi
+  # shellcheck disable=SC1091
+  source .venv/bin/activate
+
+  # .venv/bin/pip install -r requirements.txt
+  .venv/bin/pip install pre-commit
+
+else
+  # shellcheck disable=SC1091
+  source .venv/bin/activate
+fi
+
+pre-commit run --all-files
