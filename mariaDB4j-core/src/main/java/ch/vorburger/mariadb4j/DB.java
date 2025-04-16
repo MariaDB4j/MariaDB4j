f18377de186d7cadade430282a81079eab40c468
Cristian Ghezzi <cristian@ghezzi.net>
2025-04-16 13:08:54 +0200

    Open existing database

--- mariaDB4j-core/src/main/java/ch/vorburger/mariadb4j/DB.java
+++ mariaDB4j-core/src/main/java/ch/vorburger/mariadb4j/DB.java
@@ -82,6 +82,35 @@ public class DB {
     }
 
     /**
+     * This factory method is the mechanism for opening an existing embedded database for use. This
+     * method assumes that the database has already been prepared for use.
+     *
+     * @param config Configuration of the embedded instance
+     * @return a new DB instance
+     * @throws ManagedProcessException if something fatal went wrong
+     */
+    public static DB openEmbeddedDB(DBConfiguration config) throws ManagedProcessException {
+        DB db = new DB(config);
+        db.prepareDirectories();
+        return db;
+    }
+
+    /**
+     * This factory method is the mechanism for opening an existing embedded database for use. This
+     * method assumes that the database has already been prepared for use with default
+     * configuration, allowing only for specifying port.
+     *
+     * @param port the port to start the embedded database on
+     * @return a new DB instance
+     * @throws ManagedProcessException if something fatal went wrong
+     */
+    public static DB openEmbeddedDB(int port) throws ManagedProcessException {
+        DBConfigurationBuilder config = DBConfigurationBuilder.newBuilder();
+        config.setPort(port);
+        return openEmbeddedDB(config.build());
+    }
+    
+    /**
      * This factory method is the mechanism for constructing a new embedded database for use. This
      * method automatically installs the database and prepares it for use.
      *
@@ -90,13 +119,13 @@ public class DB {
      * @throws ch.vorburger.exec.ManagedProcessException if something fatal went wrong
      */
     public static DB newEmbeddedDB(DBConfiguration config) throws ManagedProcessException {
-        DB db = new DB(config);
-        db.prepareDirectories();
-        db.unpackEmbeddedDb();
-        db.install();
-        return db;
+    	DB db = new DB(config);
+    	db.prepareDirectories();
+    	db.unpackEmbeddedDb();
+    	db.install();
+    	return db;
     }
-
+    
     /**
      * This factory method is the mechanism for constructing a new embedded database for use. This
      * method automatically installs the database and prepares it for use with default
@@ -107,9 +136,9 @@ public class DB {
      * @throws ch.vorburger.exec.ManagedProcessException if something fatal went wrong
      */
     public static DB newEmbeddedDB(int port) throws ManagedProcessException {
-        DBConfigurationBuilder config = new DBConfigurationBuilder();
-        config.setPort(port);
-        return newEmbeddedDB(config.build());
+    	DBConfigurationBuilder config = new DBConfigurationBuilder();
+    	config.setPort(port);
+    	return newEmbeddedDB(config.build());
     }
 
     protected ManagedProcess createDBInstallProcess() throws ManagedProcessException, IOException {
